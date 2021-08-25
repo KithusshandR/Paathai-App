8e9d838e649836c9aa6f5a6fc4e34eac14ccd8c1
KithusshandR <KithusshandR>
2021-08-25 19:41:13 +0530

    'getAddress'

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -33,6 +34,8 @@ class _MapViewState extends State<MapView> {
   late GoogleMapController mapController;
 
   late Position _currentPosition;
+  String _currentAddress = '';
+
   _getCurrentLocation() async {
     await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
         .then((Position position) async {
@@ -48,7 +51,7 @@ class _MapViewState extends State<MapView> {
           ),
         );
       });
-      // await _getAddress();
+      await _getAddress();
     }).catchError((e) {
       print(e);
     });
@@ -58,6 +61,27 @@ class _MapViewState extends State<MapView> {
   void initState() {
     super.initState();
     _getCurrentLocation();
+  }
+
+  final startAddressController = TextEditingController();
+  String _startAddress = '';
+
+  _getAddress() async {
+    try {
+      List<Placemark> p = await placemarkFromCoordinates(
+          _currentPosition.latitude, _currentPosition.longitude);
+
+      Placemark place = p[0];
+
+      setState(() {
+        _currentAddress =
+            "${place.name}, ${place.locality}, ${place.postalCode}, ${place.country}";
+        startAddressController.text = _currentAddress;
+        _startAddress = _currentAddress;
+      });
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
@@ -79,41 +103,193 @@ class _MapViewState extends State<MapView> {
 
               mapType: MapType.normal,
               //zoomGesturesEnabled: true,
-              zoomControlsEnabled: true,
+              zoomControlsEnabled: false,
               onMapCreated: (GoogleMapController controller) {
                 mapController = controller;
               },
             ),
-            Positioned(
-                bottom: 100,
-                right: 10,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Material(
-                    color: Colors.white, // button color
-                    child: InkWell(
-                      splashColor: Colors.blue, // inkwell color
-                      child: SizedBox(
-                        width: 40,
-                        height: 40,
-                        child: Icon(Icons.my_location),
+            SafeArea(
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white70,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20.0),
                       ),
-                      onTap: () {
-                        mapController.animateCamera(
-                          CameraUpdate.newCameraPosition(
-                            CameraPosition(
-                              target: LatLng(
-                                _currentPosition.latitude,
-                                _currentPosition.longitude,
+                    ),
+                    width: width * 0.9,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                         
+                          Container(
+                            width: width * 0.8,
+                            child: TextField(
+                              onChanged: (value) {
+                                setState(() {
+                                 _startAddress = value;
+                                });
+                              },
+                              
+                              decoration: new InputDecoration(
+                                filled: true,
+                                fillColor: Colors.white,
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(10.0),
+                                  ),
+                                  borderSide: BorderSide(
+                                    color: Colors.grey.shade400,
+                                    width: 2,
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(10.0),
+                                  ),
+                                  borderSide: BorderSide(
+                                    color: Colors.blue.shade300,
+                                    width: 2,
+                                  ),
+                                ),
+                                contentPadding: EdgeInsets.all(15),
+                                hintText: 'Choose 1',
+                                 suffixIcon: IconButton(
+                                icon: Icon(Icons.search),
+                                onPressed: () {
+                                  _getAddress();
+                                  startAddressController.text = _currentAddress;
+                                  _startAddress = _currentAddress;
+                                },
                               ),
-                              zoom: 18.0,
+                              ),
+                               controller: startAddressController,
                             ),
                           ),
-                        );
-                      },
+                        ],
+                      ),
                     ),
                   ),
-                )),
+                ),
+              ),
+            ),
+            SafeArea(
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    bottom: 20,
+                    right: 20,
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Material(
+                      color: Colors.white, // button color
+                      child: InkWell(
+                        // splashColor: Colors.orange, // inkwell color
+                        child: SizedBox(
+                          width: 40,
+                          height: 40,
+                          child: Icon(Icons.my_location),
+                        ),
+                        onTap: () {
+                          print('CURRENT Address: $_currentAddress');
+                          mapController.animateCamera(
+                            CameraUpdate.newCameraPosition(
+                              CameraPosition(
+                                target: LatLng(
+                                  _currentPosition.latitude,
+                                  _currentPosition.longitude,
+                                ),
+                                zoom: 15.0,
+                              ),
+                            ),
+                          );
+                        },
+                        onDoubleTap: () {
+                          mapController.animateCamera(
+                            CameraUpdate.newCameraPosition(
+                              CameraPosition(
+                                target: LatLng(
+                                  _currentPosition.latitude,
+                                  _currentPosition.longitude,
+                                ),
+                                zoom: 10.0,
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SafeArea(
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    bottom: 20,
+                    right: 20,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(10),
+                          topLeft: Radius.circular(10),
+                        ),
+                        child: Material(
+                          color: Colors.white, // button color
+                          child: InkWell(
+                            //splashColor: Colors.blue, // inkwell color
+                            child: SizedBox(
+                              width: 40,
+                              height: 40,
+                              child: Icon(Icons.add),
+                            ),
+                            onTap: () {
+                              mapController.animateCamera(
+                                CameraUpdate.zoomIn(),
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(10),
+                          bottomLeft: Radius.circular(10),
+                        ),
+                        child: Material(
+                          color: Colors.white, // button color
+                          child: InkWell(
+                            // splashColor: Colors.blue, // inkwell color
+                            child: SizedBox(
+                              width: 40,
+                              height: 40,
+                              child: Icon(Icons.remove),
+                            ),
+                            onTap: () {
+                              mapController.animateCamera(
+                                CameraUpdate.zoomOut(),
+                              );
+                            },
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
